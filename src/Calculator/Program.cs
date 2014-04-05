962a120b4018acfdd193021f370fabb72a83211f
Wesley McClure <wes.mcclure@gmail.com>
2014-04-05 11:23:17 -0700

    Console CI demo - failing test - console app that can be tested (package result) - naming issue on public method for inspections - readkey make failing demos pause, will use these most often

---
+++ src/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator
+{
+	using System;
+
+	public class Program
+	{
+		private static void Main(string[] numbers)
+		{
+			if (numbers.Length != 2)
+			{
+				Console.WriteLine("Error: please input two numbers to add");
+				Console.ReadKey();
+				return;
+			}
+			var result = add(Convert.ToInt32(numbers[0]), Convert.ToInt32(numbers[1]));
+			Console.WriteLine("{0} + {1} = {2}", numbers[0], numbers[1], result);
+		}
+
+		public static int add(int a, int b)
+		{
+			// intentional bug to fix
+			return a - b;
+		}
+	}
+}
---
+++ src/Tests/AdditionTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Tests
+{
+	using Calculator;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class AdditionTests : AssertionHelper
+	{
+		[Test]
+		public void AddPositiveNumbers()
+		{
+			var result = Program.add(1, 2);
+
+			Expect(result, Is.EqualTo(3));
+		}
+	}
+}
