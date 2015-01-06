e62a9031156bc5f6de6c79d0a5198a48d0cf89a1
Wesley McClure <wes.mcclure@gmail.com>
2015-01-06 15:52:43 -0500

    Add testing of adding positive and negative numbers

--- src/Tests/AdditionTests.cs
+++ src/Tests/AdditionTests.cs
@@ -21,5 +21,13 @@
 
 			Expect(result, Is.EqualTo(-3));
 		}
+
+		[Test]
+		public void AddPositiveAndNegativeNumbers()
+		{
+			var result = Program.add(1, -2);
+
+			Expect(result, Is.EqualTo(-1));
+		}
 	}
 }
