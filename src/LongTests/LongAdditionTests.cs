2007d2b7300a05a83b15a295cd0416b1e5261d61
Wesley McClure <wes.mcclure@gmail.com>
2015-01-06 15:48:22 -0500

    Add lengthy tests for demoing long/short test partitioning in CI

---
+++ src/LongTests/LongAdditionTests.cs
@@ -0,0 +1,15 @@
+﻿namespace LongTests
+{
+	using System.Threading;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class LongAdditionTests : AssertionHelper
+	{
+		[Test]
+		public void RunThroughOneMillionIterations()
+		{
+			Thread.Sleep(5000);
+		}
+	}
+}
